cd2a5470225975fd3bd7478cfaec5711f1a7f023
jz20 <jianz03@outlook.com>
2021-08-24 12:49:00 +0200

    complete move parser

---
+++ test/parser/moveparsertest.h
@@ -0,0 +1,13 @@
+#ifndef MOVE_PARSER_TEST_H
+#define MOVE_PARSER_TEST_H
+
+// test the header is created correctly
+bool moveHeaderTest();
+
+// test the cpp implementation is created correctly
+bool moveImplTest();
+
+// run the tests and output the results
+bool runMoveParserTests();
+
+#endif
